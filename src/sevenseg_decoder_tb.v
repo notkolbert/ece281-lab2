c3ffd7a4c10be000e09fa6838c9eddc06e1be483
notkolbert <145070821+notkolbert@users.noreply.github.com>
2025-02-25 15:48:26 -0700

    new file

---
+++ src/sevenseg_decoder_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/25/2025 03:46:10 PM
+// Design Name: 
+// Module Name: sevenseg_decoder_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sevenseg_decoder_tb(
+    input [3:0] i_Hex,
+    output [7:0] o_seg_n
+    );
+endmodule
